e142c48f8ab812de9085b96de8abb78069edbfd9
Mohamad Shamia <Mshamia1988@gmail.com>
2023-01-26 14:43:48 +0300

    Add files via upload

---
+++ HangFireBL/IJobTestService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangFireBL
+{
+    public interface IJobTestService
+    {
+        void FireAndForgetJob();
+        void ReccuringJob();
+        void DelayedJob();
+        void ContinuationJob();
+    }
+}
