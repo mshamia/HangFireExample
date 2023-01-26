6bc499412afc9b2df758b3159562ba1287e90962
Mohamad Shamia <Mshamia1988@gmail.com>
2023-01-26 14:44:27 +0300

    Add files via upload

---
+++ HangFireExample/JobTestController .cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HangFireBL;
+using Hangfire;
+
+namespace HangFireExample
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobTestController : ControllerBase
+    {
+        private readonly IJobTestService _jobTestService;
+        private readonly IBackgroundJobClient _backgroundJobClient;
+        private readonly IRecurringJobManager _recurringJobManager;
+
+
+        public JobTestController(IJobTestService jobTestService, IBackgroundJobClient backgroundJobClient, IRecurringJobManager recurringJobManager)
+        {
+            _jobTestService = jobTestService;
+            _backgroundJobClient = backgroundJobClient;
+            _recurringJobManager = recurringJobManager;
+
+        }
+
+
+        [HttpGet("FireAndForgetJob")]
+        public ActionResult CreateFireAndForgetJob()
+        {
+            _backgroundJobClient.Enqueue(() => _jobTestService.FireAndForgetJob());
+            return Ok();
+        }
+
+
+        [HttpGet("DelayedJob")]
+        public ActionResult CreateDelayedJob()
+        {
+            _backgroundJobClient.Schedule(() => _jobTestService.DelayedJob(), TimeSpan.FromSeconds(60));
+            return Ok();
+        }
+
+
+
+
+        [HttpGet("ReccuringJob")]
+        public ActionResult CreateReccuringJob()
+        {
+            _recurringJobManager.AddOrUpdate("jobId", () => _jobTestService.ReccuringJob(), "0 8 * * *");
+
+
+            return Ok();
+        }
+
+
+
+        [HttpGet("ContinuationJob")]
+        public ActionResult CreateContinuationJob()
+        {
+            var parentJobId = _backgroundJobClient.Enqueue(() => _jobTestService.FireAndForgetJob());
+            _backgroundJobClient.ContinueJobWith(parentJobId, () => _jobTestService.ContinuationJob());
+
+            return Ok();
+        }
+    }
+}
--- HangFireExample/Program.cs
+++ HangFireExample/Program.cs
@@ -1,1 +1,58 @@
+using Hangfire;
+using HangFireBL;
 
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+
+builder.Services.AddMvcCore()
+        .AddApiExplorer();
+
+//This for HangFire connection String
+builder.Services.AddHangfire(x =>
+{
+    x.UseSqlServerStorage(builder.Configuration.GetConnectionString("DBConnection"));
+});
+builder.Services.AddHangfireServer();
+
+//This for Swagger
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IJobTestService, JobTestService>();
+
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HangfireApplication v1"));
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
+app.UseHangfireDashboard();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.MapRazorPages();
+
+app.Run();
